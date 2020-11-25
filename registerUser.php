4d9d569068d21a3822766c5e4d525f9772edffbe
victor silva machado <victormachadow@gmail.com>
2020-11-25 20:19:47 -0300

    cadastro com token ok

--- registerUser.php
+++ registerUser.php
@@ -1,5 +1,40 @@
 <?php
 
+$token = "13b6ac91a2";
+$data = file_get_contents('php://input');
+$json = json_decode($data, true);
+$datEmail = $json["email"];
+$datName = $json["pass"];
+
+
+echo $datEmail;
+echo $datName ;
+
+ 
+foreach (getallheaders() as $name => $value) { 
+    echo "$name: $value <br>"; 
+	 if ($name == "X-API-Key" ){
+		 
+		$clientoken = $value;
+	 }
+} 
+
+echo "Client token is :".$clientoken ;
+
+
 $strcon = mysqli_connect('localhost','victor','victor','tradegame') or die('Erro ao conectar ao banco de dados');
+
+$pegaEmail = mysqli_query( $strcon ,"SELECT * FROM user WHERE email = '$datEmail'");
+
+if(mysqli_num_rows($pegaEmail) >= 1){
+	
+	echo "Email já existe";
+	
+}
+else {
+	
+	echo "Email não existe";
+	
+}
 
 ?>

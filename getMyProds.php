16bae71b5473141ac140b2dd39bfedf094254d31
victor silva machado <victormachadow@gmail.com>
2021-01-26 17:09:38 -0300

    tokens

--- getMyProds.php
+++ getMyProds.php
@@ -1,5 +1,30 @@
 <?php
 
+$dataTok = file_get_contents('php://input');
+$jsonTok = json_decode($dataTok, true);
+$token = $jsonTok['token'];
+
+echo $token;
+
+$part = explode(".",$token);
+$header = $part[0];
+$payload = $part[1];
+$signature = $part[2];
+
+echo "this is :".$payload;
+
+$valid = hash_hmac('sha256',"$header.$payload",'minha-senha',true);
+$valid = base64_encode($valid);
+
+echo "Valid is :".$valid;
+
+if($signature == $valid){
+   echo "valid";
+   echo "Id is : ".$jsonTok['id'];
+}else{
+   echo 'invalid';
+}
+
 $strcon = mysqli_connect('localhost','victor','victor','tradegame') or die('Erro ao conectar ao banco de dados');
 
 ?>
--- removePhoto.php
+++ removePhoto.php
@@ -1,7 +1,7 @@
 <?php
 
+/*
 //$dat = $_POST["id"];
-
 $dat = file_get_contents('php://input');
 $json = json_decode($dat , true);
 var_dump($json);
@@ -29,7 +29,38 @@ array(
 echo $publishString;
 */  
 
+$dataTok = file_get_contents('php://input');
+$jsonTok = json_decode($dataTok, true);
+$token = $jsonTok['token'];
 
+echo $token;
+
+$part = explode(".",$token);
+$header = $part[0];
+$payload = $part[1];
+$signature = $part[2];
+
+echo "this is :".$payload;
+
+$valid = hash_hmac('sha256',"$header.$payload",'minha-senha',true);
+$valid = base64_encode($valid);
+
+echo "Valid is :".$valid;
+
+if($signature == $valid){
+   echo "valid";
+   echo "Id is : ".$jsonTok['id'];
+
+ // Valida token e consome api aqui //
+
+
+
+
+
+
+}else{
+   echo 'invalid';
+}
 
  
 	

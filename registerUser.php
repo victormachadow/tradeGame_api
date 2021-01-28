4b06bf10c56ba61ca6ac7e2300cde4cb2fc5aa5f
victor silva machado <victormachadow@gmail.com>
2021-01-27 21:15:49 -0300

    inserts

--- registerUser.php
+++ registerUser.php
@@ -4,27 +4,27 @@ $token = "13b6ac91a2"; // There is a simple token to register new user , for not
 $data = file_get_contents('php://input');
 $json = json_decode($data, true);
 $datEmail = $json["email"];
-$datName = $json["pass"];
+$datPass = $json["pass"];
 
 
-echo $datEmail;
-echo $datName ;
+//echo $datEmail;
+//echo $datPass ;
 
  
 foreach (getallheaders() as $name => $value) { 
-    echo "$name: $value <br>"; 
+    //echo "$name: $value <br>"; 
 	 if ($name == "X-API-Key" ){
 		 
 		$clientoken = $value;
 	 }
 } 
 
-echo "Client token is :".$clientoken ;
-echo "Token is :".$token ;
+//echo "Client token is :".$clientoken ;
+//echo "Token is :".$token ;
 
 if( $clientoken == $token ) {
 
-	echo "Acesso permitido";	
+	//echo "Acesso permitido";	
 
 	$strcon = mysqli_connect('localhost','victor','victor','tradegame1') or die('Erro ao conectar ao banco de dados');
 
@@ -32,12 +32,39 @@ if( $clientoken == $token ) {
 	
 	if(mysqli_num_rows($pegaEmail) >= 1){
 		
-		echo "Email já existe";
+		//echo "Email já existe";
+
+		$publishString = json_encode
+        (
+          array(
+			  'string' => "0" ,
+			  
+	          )
+        );
+		echo $publishString;
 		
 	}
 	else {
 		
-		echo "Email não existe";
+		//echo "Email não existe";
+
+		
+		$sql = "INSERT INTO user( email , pass ) VALUES ( '$datEmail' , '$datPass' )";
+		$result = mysqli_query($strcon,$sql) or die("Erro ao tentar cadastrar registro");
+			
+		//echo "New record has id: " . mysqli_insert_id($con);
+		if($result) {
+		$publishString = json_encode
+        (
+          array(
+			  'string' => "1" ,
+			   'id' => mysqli_insert_id($strcon)
+	          )
+        );
+		echo $publishString;
+		mysqli_close($strcon);
+		
+	    }
 		
 	}
 

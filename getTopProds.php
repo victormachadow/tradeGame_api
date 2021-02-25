680e8bbd904c872e569878458f43b56bf00b3818
victor silva machado <victormachadow@gmail.com>
2021-02-24 23:04:33 -0300

    login

--- getTopProds.php
+++ getTopProds.php
@@ -1,8 +1,66 @@
 <?php
 
-$str = file_get_contents("topProds.json");
-$json = json_decode($str, true); // decode the JSON into an associative array
-echo count($json);
+
+$data = file_get_contents('php://input');
+
+foreach (getallheaders() as $name => $value) { 
+   //echo "$name: $value <br>"; 
+   if ($name == "X-API-Key" ){
+      
+     $token = $value;
+   }
+} 
+
+//echo $token;
+
+$part = explode(".",$token);
+$header = $part[0];
+$payload = $part[1];
+$signature = $part[2];
+
+//echo "this is :".$payload;
+
+$valid = hash_hmac('sha256',"$header.$payload",'minha-senha',true);
+$valid = base64_encode($valid);
+
+//echo "Valid is :".$valid;
+
+if($signature == $valid){
+   //echo "valid";
+
+   $strcon = mysqli_connect('localhost','victor','victor','tradegame1') or die('Erro ao conectar ao banco de dados');
+
+$SQL = "SELECT id , title , code , city , valuee , price , amount FROM products ORDER by id LIMIT 0,10";
+$res = mysqli_query( $strcon , $SQL ) or die("Erro no banco de dados!"); 
+//$rows = mysqli_fetch_array($res, MYSQLI_NUM);
+//print_r ($res);
+$arrayRetorno = array();
+
+while ($row = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
+    
+    //var_dump($row['title']);
+    //echo $row;
+    //$String = (string)$row;
+    //$arrayRetorno.push($row)
+    array_push( $arrayRetorno, $row );
+
+}
+//print_r($arrayRetorno);
+
+
+$publishString = json_encode
+(
+$arrayRetorno
+);
+
+echo $publishString;
+
+   
+}else{
+   echo 'invalid';
+}
+   
+
 
 /* ( 0 , 100 ) , ( 100 , 200 ) , ( 200 , 300 ) : SELECT * FROM table LIMIT 0, 10 limit first , first+range
 
@@ -16,17 +74,8 @@ echo count($json);
  when scene prodlist reload actualPage=actualPage+range
 
 */
-//SELECT * FROM table LIMIT 0, 10  // Sql de paginaçao pega 10 registros a partir da posição 0 
+//SELECT * FROM table LIMIT 0, 10  // Sql de paginaçao pega 10 registros a partir da posição 0
 
-$publishString = json_encode
-(
-array(
-    'String' => $json,
-	'id' => "2"
-	)
-);
 
- echo $publishString;
 
- 
 ?>
--- login.php
+++ login.php
@@ -1,33 +1,100 @@
 <?php
 
-$dataTok = file_get_contents('php://input');
-$jsonTok = json_decode($dataTok, true);
-$token = $jsonTok['token'];
+$token = "13b6ac91a2"; // There is a simple token to register new user , for not having problems with strangers requests
+$data = file_get_contents('php://input');
+$json = json_decode($data, true);
+$datEmail = $json["email"];
+$datPass = $json["pass"];
 
-echo $token;
 
-$part = explode(".",$token);
-$header = $part[0];
-$payload = $part[1];
-$signature = $part[2];
+//echo $datEmail;
+//echo $datPass ;
 
-echo "this is :".$payload;
+ 
+foreach (getallheaders() as $name => $value) { 
+    //echo "$name: $value <br>"; 
+	 if ($name == "X-API-Key" ){
+		 
+		$clientoken = $value;
+	 }
+} 
 
-$valid = hash_hmac('sha256',"$header.$payload",'minha-senha',true);
-$valid = base64_encode($valid);
+function geToken()
+{
+	
+	$header = [
+		'alg' => 'HS256',
+		'typ' => 'JWT'
+	 ];
+	 $header = json_encode($header);
+	 $header = base64_encode($header);
+	 
+	 $payload = [
+		'iss' => 'localhost',
+		'name' => 'as4543434dasd',
+		'email' => 'asdaaksa'
+	 ];
+	 $payload = json_encode($payload);
+	 $payload = base64_encode($payload);
+	 
+	 $signature = hash_hmac('sha256',"$header.$payload",'minha-senha',true);
+	 $signature = base64_encode($signature);
+	 
+	 return "$header.$payload.$signature";
 
-echo "Valid is :".$valid;
-
-if($signature == $valid){
-   echo "valid";
-   echo "Id is : ".$jsonTok['id'];
-
-    // Valida token e consome api aqui //
-
-}else{
-   echo 'invalid';
 }
 
-$strcon = mysqli_connect('localhost','victor','victor','tradegame') or die('Erro ao conectar ao banco de dados');
+//echo "Client token is :".$clientoken ;
+//echo "Token is :".$token ;
+
+if( $clientoken == $token ) {
+
+   //echo "Acesso permitido";	
+   
+   $realToken = geToken();
+
+	$strcon = mysqli_connect('localhost','victor','victor','tradegame1') or die('Erro ao conectar ao banco de dados');
+
+  $SQL = "SELECT  id , name , email , pass
+  FROM user
+  WHERE email = '$datEmail' "; 
+  $result_id = mysqli_query( $strcon , $SQL ) or die("Erro no banco de dados!"); 
+  $total = mysqli_num_rows($result_id); 
+  $dados = mysqli_fetch_array($result_id); 
+
+  if(!strcmp( $datPass , $dados["pass"])) 
+  {
+
+   $publishString = json_encode
+        (
+          array(
+			  'return' => 1 ,
+			   'id' => $dados["id"],
+			   'token' => $realToken
+			   
+	          )
+        );
+		echo $publishString;
+		mysqli_close($strcon);
+ 
+
+  }
+
+
+}
+else {
+
+   $publishString = json_encode
+   (
+   array(
+       'String' => "0",
+      )
+   );
+		echo $publishString;
+		mysqli_close($strcon);
+
+	
+}	
+
 
 ?>
